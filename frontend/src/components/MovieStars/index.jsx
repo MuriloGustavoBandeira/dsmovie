1a28d7ae16ce85a9daf21f5aa08cd7b986257a78
Murilo Bandeira <murilogustavo@hotmail.com>
2022-01-12 16:38:43 -0300

    project update

--- frontend/src/components/MovieStars/index.jsx
+++ frontend/src/components/MovieStars/index.jsx
@@ -1,6 +1,6 @@
 import { ReactComponent as StarFull } from 'assets/img/star-full.svg';
 import { ReactComponent as StarEmpty } from 'assets/img/star-empty.svg';
-import { ReactComponent as StarHalf } from 'assets/img/star-half.svg';
+//import { ReactComponent as StarHalf } from 'assets/img/star-half.svg';
 import './styles.css';
 
 export default function MovieStars() {
@@ -9,7 +9,6 @@ export default function MovieStars() {
             <StarFull />
             <StarFull />
             <StarFull />
-            <StarHalf />
             <StarEmpty />
         </div>
     );
